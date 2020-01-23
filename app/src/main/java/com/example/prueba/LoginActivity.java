edee6f937d77919ee8e6ce29315b7d44e5dbdae9
Dagnos <fernando.huarcaya@tecsup.edu.pe>
2020-01-23 09:28:46 -0500

    botn flotante

--- app/src/main/java/com/example/prueba/LoginActivity.java
+++ app/src/main/java/com/example/prueba/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        Button login = findViewById(R.id.btnIngresar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
